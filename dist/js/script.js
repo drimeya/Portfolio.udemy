a4f22fb3ca99242ddd8710c03bb177f61068922b
Polina <paulinnn@yandex.ru>
2021-09-18 13:48:31 +0300

    finaled dekstop wersion

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,25 @@
 const menuHamburger = document.querySelector('.hamburger'),
       menuClose = document.querySelector('.close'),
       menu = document.querySelector('.menu');
+      menuLink = document.querySelectorAll('.menu__link');
 
-      menuHamburger.addEventListener ( 'click', () => {
-        menu.classList.add('active');
-      });
+menuHamburger.addEventListener ( 'click', () => {
+  menu.classList.add('active');
+});
 
-      menuClose.addEventListener ( 'click', () => {
-        menu.classList.remove('active');
-      });
+menuClose.addEventListener ( 'click', () => {
+  menu.classList.remove('active');
+});
+
+menuLink.forEach(item => {
+  item.addEventListener('click', () => {
+    menu.classList.remove('active');
+  });
+});
+
+const counters = document.querySelectorAll('.skills__progress-per'),
+      lines = document.querySelectorAll('.skills__unfilled span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
